5eb9e17b5b0f7c23cfb7bce7e48005c8635a5e4f
Хусайн Эсамбаев <husainesambaev@gmail.com>
2019-12-06 09:12:47 +0300

    feat(checkbox): ready

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -1,16 +1,12 @@
 import React from 'react';
 import './Checkbox.css';
 
-const Checkbox: React.FC<propTypes> = ({ name, checked, label }) => {
-  return (
-    <label className="Checkbox">
-      <input type="checkbox" name={name} checked={checked} />;
-      <span className="checkmark"></span>
-      {label}
-    </label>
-  );
+const Checkbox: React.FC<propTypes> = ({ checked }) => {
+  const classNames = `Checkbox${checked ? ' checked' : ''}`;
+
+  return <span className={classNames}></span>;
 };
 
 export default Checkbox;
 
-type propTypes = { name: string; checked: boolean; label: string };
+type propTypes = { checked?: boolean };
